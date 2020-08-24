b8895ef7ad9eb8df9fabacae7f3cffc397b2d3b9
dominicnaatey@gmail.com <dominicnaatey@gmail.com>
2020-08-24 15:02:59 +0000

    Updates

---
+++ precache-manifest.e7f57f55960ad959cb12625d39352db4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e64ac9762beb08a1f9d34c5d5bc9ddba",
+    "url": "/deployr/index.html"
+  },
+  {
+    "revision": "1eb89414eb0477cbe1dd",
+    "url": "/deployr/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "cb43c5dd6ae9c5788a7b",
+    "url": "/deployr/static/js/2.da3882de.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/deployr/static/js/2.da3882de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1eb89414eb0477cbe1dd",
+    "url": "/deployr/static/js/main.2c602d31.chunk.js"
+  },
+  {
+    "revision": "86de798d486e4326cbea",
+    "url": "/deployr/static/js/runtime-main.599a1247.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/deployr/static/media/logo.5d5d9eef.svg"
+  }
+]);
